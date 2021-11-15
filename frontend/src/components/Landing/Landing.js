7f7c924b2d536296388e43ba9f19a74a093029af
Sondinh <dnson20022001@gmail.com>
2021-11-15 22:04:46 +0700

    add hero section

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,26 +1,18 @@
 import React from "react";
 import "./Landing.css";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
+    const handleToBrowseMovies = () => {
+        window.scroll({ top: 880, behavior: "smooth" });
+    };
+
     return (
         <div className="landing-container">
-            <div
-                style={{
-                    height: "100%",
-                    padding: "260px 50px 20px",
-                    justifyContent: "center",
-                }}
-            >
-                <h1
-                    style={{
-                        fontSize: "74px",
-                        fontWeight: "700",
-                        color: "#7975ec",
-                    }}
-                >
-                    ADVENTURE AWAITS
-                </h1>
+            <div>
+                <h1 className="landing-title">ADVENTURE AWAITS</h1>
                 <p
+                    className="landing-small-title"
                     style={{
                         fontSize: "40px",
                         fontWeight: "700",
@@ -28,17 +20,16 @@ const Landing = () => {
                 >
                     What are you waiting for?
                 </p>
+                <Link to="/sign-up">
+                    <button className="landing-getStarted-btn landing-btn">
+                        Get Started
+                    </button>
+                </Link>
                 <button
-                    style={{
-                        padding: "10px 15px",
-                        backgroundColor: "#7975ec",
-                        borderRadius: "4px",
-                        color: "#e9e9eb",
-                        fontSize: "24px",
-                        marginTop: "15px",
-                    }}
+                    className="landing-toBrowse-btn landing-btn"
+                    onClick={() => handleToBrowseMovies()}
                 >
-                    START NOW
+                    Browse Movies
                 </button>
             </div>
         </div>
